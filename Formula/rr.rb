0c72f91b2315273ad5731d3bbe96350a488f68e8
Austin Graham <austin_graham@intuit.com>
2022-09-11 23:53:39 -0700

    fix hash

--- Formula/rr.rb
+++ Formula/rr.rb
@@ -2,7 +2,7 @@ class Rr < Formula
   desc "CLI for managing a local Redis cluster"
   homepage "https://github.com/austinpgraham/rusty-redis"
   url "https://github.com/austinpgraham/rusty-redis/releases/download/v0.1.0/rr-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "5a7c2211d031f27a25d238e49705d7033a2c772e"
+  sha256 "e6442132dd6cbb5d134b94f97e62881d52505f5a19c1d45cf25d6cc3a985f1d0"
   version "0.1.0"
 
   def install
